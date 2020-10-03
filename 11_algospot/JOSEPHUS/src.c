03a414995533708570c704709e563e2b6b66e45d
mikasa1991 <42025832+mikasa1991@users.noreply.github.com>
2020-10-03 23:26:43 +0900

    [JOSEPHUS]https://www.algospot.com/judge/problem/read/JOSEPHUS [연산횟수]N*K [자료구조]linkedlist [알고리즘] [제출결과]Success [회고] 1.기존대비 가독성을 올렸다. List구조체 내에서 변수 및 함수를 정의하도록 변경함.

--- 11_algospot/JOSEPHUS/src.c
+++ 11_algospot/JOSEPHUS/src.c
@@ -5,60 +5,62 @@
 #include <stdio.h>
 #include <assert.h>
 
-typedef struct _List
-{
-    int num;
-    _List* prev;
-    _List* next;
-}List;
-
 const int SURVIVOR_CNT = 2;
 const int MAX_NODE_CNT = (1000 + 1);
-List g_astNode[MAX_NODE_CNT];
-int gIndex;
 
-void initializeList(List* pHead, List* pTail)
+typedef struct _Node
 {
-    gIndex = 0;
-    pHead->prev = NULL;
-    pHead->next = pTail;
-    pTail->prev = pHead;
-    pTail->next = NULL;
-}
+    int num;
+    _Node* prev;
+    _Node* next;
+}Node;
 
-List* makeAllocNode(int num)
+typedef struct _List
 {
-    List* pNewNode;
-    pNewNode = &g_astNode[gIndex++];
-    pNewNode->num = num;
-    pNewNode->prev = NULL;
-    pNewNode->next = NULL;
-    return pNewNode;
-}
-
-void insertNode(List* pNode, int num)
-{
-    List* pNewNode = makeAllocNode(num);
-    pNewNode->prev = pNode->prev;
-    pNewNode->next = pNode;
-    pNode->prev->next = pNewNode;
-    pNode->prev = pNewNode;
-}
-
-void printNode(List* pHead, List* pTail)
-{
-    for (List* t = pHead->next; t != pTail; t = t->next)
+    Node stNodeNotUsed[MAX_NODE_CNT];
+    int idx;
+    Node stHead;
+    Node stTail;
+    void initialize()
     {
-        printf("%d ", t->num);
+        idx = 0;
+        stHead.prev = NULL;
+        stHead.next = &stTail;
+        stTail.prev = &stHead;
+        stTail.next = NULL;
     }
-    printf("\n");
-}
-
-void deleteNode(List* pNode)
-{
-    pNode->prev->next = pNode->next;
-    pNode->next->prev = pNode->prev;
-}
+    Node* allocNode(int num)
+    {
+        assert(idx < MAX_NODE_CNT);
+        Node* pNewNode;
+        pNewNode = &stNodeNotUsed[idx++];
+        pNewNode->num = num;
+        pNewNode->prev = NULL;
+        pNewNode->next = NULL;
+        return pNewNode;
+    }
+    void insertNode(Node* pNode, int num)
+    {
+        Node* pNewNode = allocNode(num);
+        pNewNode->prev = pNode->prev;
+        pNewNode->next = pNode;
+        pNode->prev->next = pNewNode;
+        pNode->prev = pNewNode;
+    }
+    void printNode()
+    {
+        for (Node* t = stHead.next; t != &stTail; t = t->next)
+        {
+            printf("%d ", t->num);
+        }
+        printf("\n");
+    }
+    void deleteNode(Node* pNode)
+    {
+        pNode->prev->next = pNode->next;
+        pNode->next->prev = pNode->prev;
+    }
+}List;
 
 int main()
 {
@@ -71,36 +73,35 @@ int main()
         int N, K;
         scanf("%d%d", &N, &K);
 
-        List Head;
-        List Tail;
-        initializeList(&Head, &Tail);
+        List stList;
+        stList.initialize();
 
         //linkedlist 형태로 삽입한다.
         for (int i = 1; i <= N; i++)    //to delete 1, i just didn't insert that node.
         {
-            insertNode(&Tail, i);
+            stList.insertNode(&stList.stTail, i);
         }
 
         //1번 Node를 죽인다.
-        List* pNode = Head.next;
-        deleteNode(pNode);
+        Node* pNode = stList.stHead.next;
+        stList.deleteNode(pNode);
 
         //Loop: 생존자가 2명이하일 때, 탈출한다.
         //Do  : K번 이동한다. 죽인다.
-        for (int listCnt = N-1; listCnt > SURVIVOR_CNT; listCnt--)
+        for (int listCnt = N - 1; listCnt > SURVIVOR_CNT; listCnt--)
         {
             for (int i = 0; i < K; i++)
             {
-                if (pNode->next == &Tail)
-                    pNode = Head.next;
+                if (pNode->next == &stList.stTail)
+                    pNode = stList.stHead.next;
                 else
                     pNode = pNode->next;
             }
-            deleteNode(pNode);
+            stList.deleteNode(pNode);
         }
 
         //생존자 2명을 출력한다.
-        printNode(&Head, &Tail);
+        stList.printNode();
     }
     return 0;
 }
