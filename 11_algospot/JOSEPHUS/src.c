8075b9787b609d02febba952bb5455da62ff01c6
mikasa1991 <42025832+mikasa1991@users.noreply.github.com>
2020-10-04 00:06:22 +0900

    [JOSEPHUS]https://www.algospot.com/judge/problem/read/JOSEPHUS [연산횟수]N*K [자료구조]Queue [알고리즘] [제출결과]Success [회고] 1.Queue로 구현할 경우, 생존자 2명이 남았을 때, Queue에 오름차순으로 존재하지 않을 수가 있다. 따라서, 크기비교를 통해 오름차순으로 출력해야한다. 2.linkedlist보다 구현 측면에서 비교하자면 조금 더 쉬운 편이다. 왜냐하면, 인원 한 바퀴를 돌았을 때, 어떻게 해야하냐 고민할 필요가 없다.

--- 11_algospot/JOSEPHUS/src.c
+++ 11_algospot/JOSEPHUS/src.c
@@ -5,8 +5,11 @@
 #include <stdio.h>
 #include <assert.h>
 
+#define MAX(a, b) (a > b) ? a : b
+#define MIN(a, b) (a < b) ? a : b
 const int SURVIVOR_CNT = 2;
 const int MAX_NODE_CNT = (1000 + 1);
+const int QUEUE_SIZE = (1000 + 10);
 
 typedef struct _Node
 {
@@ -67,8 +70,47 @@ typedef struct _List
     }
 }List;
 
+typedef struct _Queue
+{
+    int queueCnt;
+    int head;
+    int tail;
+    int array[QUEUE_SIZE];
+    void init()
+    {
+        head = 0;
+        tail = 0;
+        queueCnt = 0;
+    }
+    void push(int num)
+    {
+        assert(false == IsFull());
+        array[tail] = num;
+        tail = (tail + 1) % QUEUE_SIZE;
+        queueCnt++;
+    }
+    int pop()
+    {
+        assert(false == IsEmpty());
+        int temp = array[head];
+        head = (head + 1) % QUEUE_SIZE;
+        queueCnt--;
+        return temp;
+    }
+    bool IsEmpty()
+    {
+        return (head == tail);
+    }
+    bool IsFull()
+    {
+        return ((tail + 1) % QUEUE_SIZE == head);
+    }
+}Queue;
+
 int main()
 {
+#define METHOD 2
+#if (METHOD == 1) //List
     freopen("input.txt", "r", stdin);
     int C;
     scanf("%d", &C);
@@ -105,4 +147,36 @@ int main()
         stList.printNode();
     }
     return 0;
+#elif (2 == METHOD) //Queue
+    {
+        freopen("input.txt", "r", stdin);
+        int C;
+        scanf("%d", &C);
+        for (int i = 0; i < C; i++)
+        {
+            int N, K;
+            scanf("%d%d", &N, &K);
+            Queue stQueue;
+            stQueue.init();
+            for (int i = 1; i <= N; i++)
+            {
+                stQueue.push(i);
+            }
+            while (1)
+            {
+                stQueue.pop();
+                if (SURVIVOR_CNT == stQueue.queueCnt) break;
+                for (int i = 0; i < K - 1; i++)
+                {
+                    int temp = stQueue.pop();
+                    stQueue.push(temp);
+                }
+            }
+            int t1 = stQueue.pop();
+            int t2 = stQueue.pop();
+            printf("%d %d\n", MIN(t1, t2), MAX(t1, t2));
+        }
+        return 0;
+    }
+#endif
 }
