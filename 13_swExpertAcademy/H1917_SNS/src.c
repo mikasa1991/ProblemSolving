fecd63dcf3d22a5b1efb346d0fa2bcd510ad3663
mikasa1991 <42025832+mikasa1991@users.noreply.github.com>
2021-06-19 22:57:44 +0900

    [H1917_SNS]https://out.swexpertacademy.samsung.com/common/swea/solvingPractice/problemDetail.do?contestProbId=AWxQBnaf4YkAAAHb [자료구조]LinkedList, Hash(Chaining), mergeSort [알고리즘] [제출결과]Success [회고] 이전 풀이와 다른 점은, 1.Hash는 RefCode방식보다는 chaining 방식으로 구현하는 것이 충돌나는 경우에 시간복잡도면에서 유리하다. 2.getMessages함수 구현 시, 이전에는 heap으로 구현했으나, mergeSort로 구현했다.

--- 13_swExpertAcademy/H1917_SNS/src.c
+++ 13_swExpertAcademy/H1917_SNS/src.c
@@ -1,8 +1,18 @@
-// https://out.swexpertacademy.samsung.com/common/swea/solvingPractice/problemDetail.do?contestProbId=AWxQBnaf4YkAAAHb
-//#include <assert.h>
 
-#define MAX_USER      1000
-#define MAX_TAG       5000
+#define MAX_USER      (1000+10)
+#define MAX_TAG       (5000+10)
+#define TIME_STR_NUM  (9)
+#define STR_NUM       (10)
+#define MAX_HASH      (5000+10)
+#define MAX_ARR       ((5 * (int)1e5) + 10)
+#define MAX_MSG       ((int)1e6 + 10)
+#define NULL          (0)
+#define MAX_NODE      ((6 * (int)1e5) + 10)
+int gFollowTb[MAX_USER][MAX_USER];
+
+// mergeSort buffer
+int gArray[MAX_ARR];
+int gBuffer[MAX_ARR];
 
 // The below commented functions are for your reference. If you want 
 // to use it, uncomment these functions.
@@ -14,144 +24,189 @@ int mstrcmp(const char *a, const char *b)
     return a[i] - b[i];
 }
 
-int mstrcpy(char *dest, const char *src)
+void mstrcpy(char *dest, const char *src)
 {
     int i = 0;
-    while (src[i] != '\0' && src[i] != ' ') { dest[i] = src[i]; i++; }
-    if (src[i] == '\0')
-    {
-        dest[i] = src[i];
-    }
-    return i;
+    while (src[i] != '\0') { dest[i] = src[i]; i++; }
+    dest[i] = src[i];
 }
-
-#define NULL            (0)
-#define MAX_MSG_NUM     ((int)1e6+1)
-#define MAX_HASHTAG_NUM (10000+1)
-#define MAX_USER_NUM    (1000+1)
-#define MAX_NODE        ((int)1e7)
-#define MAX_HASHTAG_LEN (9 + 1)
 
 struct Msg
 {
-    int userId;
     int time;
+    int userId;
 
     void init()
     {
-        userId = 0;
         time = 0;
+        userId = 0;
     }
 };
+
+Msg gMsgTb[MAX_MSG];
+
+// 최근일수록, userId가 작을수록 우선순위가 높다.
+int compare(int a, int b)
+{
+    int aTime = gMsgTb[a].time;
+    int bTime = gMsgTb[b].time;
+
+    if (aTime == bTime)
+    {
+        return (gMsgTb[a].userId < gMsgTb[b].userId);
+    }
+    else
+    {
+        return (aTime > bTime);
+    }
+}
 
 struct Node
 {
     int msgId;
     Node* prev;
     Node* next;
-
-#define NOT_DEFINED (-1)
+    
     void init()
     {
-        msgId = NOT_DEFINED;
+        msgId = -1;
         prev = NULL;
         next = NULL;
     }
 
-    Node* alloc()
+    Node* alloc(int msgId)
     {
+        this->msgId = msgId;
         return this;
     }
 };
 
-Node gNodePool[MAX_NODE];
+Node gFreeNode[MAX_NODE];
 int gUsedCnt;
-Msg gMsgTb[MAX_MSG_NUM];
-
-// time이 클수록, time이 같다면 유저 id가 작을수록
-int compare(int a, int b)
-{
-    Msg aMsg;
-    Msg bMsg;
-    aMsg.time = gMsgTb[a].time;
-    aMsg.userId = gMsgTb[a].userId;
-    bMsg.time = gMsgTb[b].time;
-    bMsg.userId = gMsgTb[b].userId;
-
-    int bRet = 0;
-    if (aMsg.time > bMsg.time)
-    {
-        bRet = 1;
-    }
-    else if (aMsg.time < bMsg.time)
-    {
-        bRet = 0;
-    }
-    else // (aMsg.time == bMsg.time)
-    {
-        if (aMsg.userId < bMsg.userId)
-        {
-            bRet = 1;
-        }
-        else if (aMsg.userId > bMsg.userId)
-        {
-            bRet = 0;
-        }
-        else
-        {
-            //assert(0);
-        }
-    }
-    return bRet;
-}
 
 struct List
 {
     Node head;
     Node tail;
-    int cnt;
+    Node* cur;
+    int nodeCnt;
 
     void init()
     {
-        cnt = 0;
-
+        cur = head.next;
+        nodeCnt = 0;
         head.init();
         tail.init();
         head.next = &tail;
         tail.prev = &head;
     }
 
-    int insertByOrder(int msgId)
+    void addNode(int msgId)
     {
-        int bRet = 0;
-
+        bool bNeedToAdd = true;
+        int searchCnt;
         Node* temp;
-        int searchCnt = 0;
-        for (temp = head.next; temp != &tail; temp = temp->next, searchCnt++)
+
+        // 우선순위가 상위 5개보다 낮으면, node를 추가할 필요가 없다.
+        // 그 외는 node를 추가해야한다.
+        for (searchCnt = 0, temp = head.next; temp != &tail; temp = temp->next, searchCnt++)
         {
-            if (searchCnt >= 5) // 나보다 우선순위 높은 노드가 5개 있다면,
+            if (searchCnt > 5)
             {
-                return 0;
-            }
-            if (compare(msgId, temp->msgId))
-            {
-                bRet = 1;
+                bNeedToAdd = false;
                 break;
+            }
+            else
+            {
+                if (compare(msgId, temp->msgId))
+                {
+                    break;
+                }
             }
         }
 
-        Node* newNode = gNodePool[gUsedCnt++].alloc();
-        newNode->msgId = msgId;
-        newNode->next = temp;
-        newNode->prev = temp->prev;
-        temp->prev->next = newNode;
-        temp->prev = newNode;
-        cnt++;
-        return bRet;
+        if (true == bNeedToAdd)
+        {
+            Node* pNode = gFreeNode[gUsedCnt++].alloc(msgId);
+            
+            pNode->next = temp;
+            pNode->prev = temp->prev;
+            temp->prev->next = pNode;
+            temp->prev = pNode;
+            
+            nodeCnt++;
+        }
     }
 };
 
-List gUserTb[MAX_USER_NUM];
+List gUserListTb[MAX_USER];
+List gTagListTb[MAX_TAG];
+int gId;
+
+struct hashNode
+{
+    char key[STR_NUM];
+    int data; // id
+    hashNode* prev;
+    hashNode* next;
+
+    void init()
+    {
+        data = 0;
+        prev = NULL;
+        next = NULL;
+    }
+
+    hashNode* alloc(char* key, int data)
+    {
+        mstrcpy(this->key, key);
+        this->data = data;
+        return this;
+    }
+};
+
+hashNode gFreeHashNode[MAX_HASH];
+int gUsedHashNodeCnt;
+
+struct hashList
+{
+    hashNode head;
+    hashNode tail;
+    int nodeCnt;
+
+    void init()
+    {
+        head.init();
+        tail.init();
+        head.next = &tail;
+        tail.prev = &head;
+        nodeCnt = 0;
+    }
+
+    int getData(char* str)
+    {
+        // str이 없으면, add하고 return.
+        // str이 있으면, return.
+        for (hashNode* temp = head.next; temp != &tail; temp = temp->next)
+        {
+            if (0 == mstrcmp(temp->key, str)) // str이 있으면, return.
+            {
+                return temp->data;
+            }
+        }
+
+        hashNode* pNode = gFreeHashNode[gUsedHashNodeCnt++].alloc(str, gId++);
+        pNode->next = head.next;
+        pNode->prev = &head;
+        head.next->prev = pNode;
+        head.next = pNode;
+        nodeCnt++;
+        
+        return pNode->data;
+    }
+};
+
+hashList gHashListTb[MAX_HASH];
 
 unsigned long hash(const char* str)
 {
@@ -160,255 +215,184 @@ unsigned long hash(const char* str)
 
     while (c = *str++)
     {
-        hash = (((hash << 5) + hash) + c) % MAX_HASHTAG_NUM;
+        hash = (((hash << 5) + hash) + c) % MAX_HASH;
     }
 
-    return hash % MAX_HASHTAG_NUM;
+    return hash % MAX_HASH;
 }
 
-struct Hash
-{
-    char key[MAX_HASHTAG_LEN];
-    List list;
-
-    void init()
-    {
-        for (int i = 0; i < MAX_HASHTAG_LEN; i++)
-        {
-            key[i] = 0;
-            list.init();
-        }
-    }
-};
-Hash gHashTb[MAX_HASHTAG_NUM];
-
-int find(const char* _key)
-{
-    unsigned long h = hash(_key);
-    int cnt = MAX_HASHTAG_NUM;
-
-    while (gHashTb[h].key[0] != 0 && cnt--)
-    {
-        if (mstrcmp(gHashTb[h].key, _key) == 0)
-        {
-            return h;
-        }
-        h = (h + 1) % MAX_HASHTAG_NUM;
-    }
-    return -1;
-}
-
-int add(const char* _key)
-{
-    unsigned long h = hash(_key);
-
-    while (gHashTb[h].key[0] != 0)
-    {
-        if (mstrcmp(gHashTb[h].key, _key) == 0) //기존에 등록된 key라면,
-        {
-            return h;
-        }
-
-        h = (h + 1) % MAX_HASHTAG_NUM;
-    }
-    mstrcpy(gHashTb[h].key, _key);
-    return h;
-}
-#define MAX_HEAP_SIZE (MAX_USER_NUM * 10)
-
-struct Heap
-{
-    int heap[MAX_HEAP_SIZE] = { 0, }; //msgId
-    int heapSize = 0;
-
-    void init(void)
-    {
-        heapSize = 0;
-    }
-
-    int heapPush(int value)
-    {
-        if (heapSize + 1 > MAX_HEAP_SIZE)
-        {
-            return 0;
-        }
-
-        heap[heapSize] = value;
-
-        int current = heapSize;
-        while (current > 0 && compare(heap[current], heap[(current - 1) / 2]))
-        {
-            int temp = heap[(current - 1) / 2];
-            heap[(current - 1) / 2] = heap[current];
-            heap[current] = temp;
-            current = (current - 1) / 2;
-        }
-
-        heapSize = heapSize + 1;
-
-        return 1;
-    }
-
-    int heapPop(int* value)
-    {
-        if (heapSize <= 0)
-        {
-            return -1;
-        }
-
-        *value = heap[0];
-        heapSize = heapSize - 1;
-
-        heap[0] = heap[heapSize];
-
-        int current = 0;
-        while (current * 2 + 1 < heapSize)
-        {
-            int child;
-            if (current * 2 + 2 == heapSize)
-            {
-                child = current * 2 + 1;
-            }
-            else
-            {
-                child = compare(heap[current * 2 + 1], heap[current * 2 + 2]) ? current * 2 + 1 : current * 2 + 2;
-            }
-
-            if (compare(heap[current], heap[child]))
-            {
-                break;
-            }
-
-            int temp = heap[current];
-            heap[current] = heap[child];
-            heap[child] = temp;
-
-            current = child;
-        }
-        return 1;
-    }
-};
-
-int gFollowTb[MAX_USER_NUM][MAX_USER_NUM];
-Heap gHeap;
 void init()
 {
     gUsedCnt = 0;
-    for (int i = 0; i < MAX_MSG_NUM; i++)
+    gId = 0;
+    gUsedHashNodeCnt = 0;
+    for (int i = 0; i < MAX_MSG; i++)
     {
         gMsgTb[i].init();
     }
-    for (int i = 0; i < MAX_USER_NUM; i++)
+    for (int i = 0; i < MAX_USER; i++)
     {
-        gUserTb[i].init();
+        gUserListTb[i].init();
+        gFollowTb[i][0] = 0;
     }
-    for (int i = 0; i < MAX_HASHTAG_NUM; i++)
+    for (int i = 0; i < MAX_TAG; i++)
     {
-        gHashTb[i].init();
+        gTagListTb[i].init();
     }
-    for (int i = 0; i < MAX_USER_NUM; i++)
+    for (int i = 0; i < MAX_HASH; i++)
     {
-        for (int j = 0; j < MAX_USER_NUM; j++)
-        {
-            gFollowTb[i][j] = 0;
-        }
+        gHashListTb[i].init();
     }
-    gHeap.init();
 }
-// msgTb에 userId, time을 등록한다. (hashTag는 굳이...)
-// userTb에 Node(msgId)를 우선순위를 고려하여 삽입한다. 최대 5개까지.
-// hashtagTb에 Node(msgId)를 우선순위를 고려하여 삽입한다. 최대 5개까지.
+
+#define TIME_NUM (8)
+int getTime(char* pTime)
+{
+    int nTime = 0;
+    int a = 100000;
+    for (int i = 0; i < TIME_NUM; i++)
+    {
+        if (':' == pTime[i])
+        {
+            continue;
+        }
+        nTime += (pTime[i] - '0') * a;
+        a = a / 10;
+    }
+    return nTime;
+}
+
+void copyTag(char* pDest, char* pSrc, int* pIndex)
+{
+    int i;
+    for (i = 0; (' ' != pSrc[i]) && (0 != pSrc[i]); i++)
+    {
+        pDest[i] = pSrc[i];
+    }
+    *pIndex += i;
+}
+
 void createMessage(int msgID, int userID, char msgData[])
 {
+    // msgTb
     gMsgTb[msgID].userId = userID;
+    gMsgTb[msgID].time = getTime(msgData);
 
-#define TIME_LEN (8)
-    int num = 100000;
-    int time = 0;
-    int i;
-    for (i = 0; i < TIME_LEN; i++)
+    // userListTb
+    gUserListTb[userID].addNode(msgID);
+
+    // hashTagListTb
+    int index = TIME_STR_NUM;
+    while (0 != msgData[index])
     {
-        if (':' == msgData[i]) continue;
-        time = time + ((msgData[i]-'0') * num);
-        num = num / 10;
+        char string[STR_NUM] = { 0, };
+        if (' ' == msgData[index]) index++;
+        copyTag(string, msgData + index, &index);
+
+        int h = hash(string);
+        int id = gHashListTb[h].getData(string);
+        gTagListTb[id].addNode(msgID);
     }
-    gMsgTb[msgID].time = time;
-    gUserTb[userID].insertByOrder(msgID);
+}
 
-    while (NULL != msgData[i])
+void followUser(int userID1, int userID2)
+{
+    int* pId = &gFollowTb[userID1][0];
+    (*pId)++;
+    gFollowTb[userID1][*pId] = userID2;
+}
+
+int searchByHashtag(char tagName[], int retIDs[])
+{
+    int h = hash(tagName);
+    int id = gHashListTb[h].getData(tagName);
+    
+    int cnt = 0;
+    List* pList = &gTagListTb[id];
+    for (Node* temp = pList->head.next; temp != &(pList->tail); temp = temp->next)
     {
-        if ('#' == msgData[i])
+        if (cnt < 5)
         {
-            char string[MAX_HASHTAG_LEN] = { 0, };
-            i = i + mstrcpy(string, &msgData[i]);
-            int idx = add(string);
-            List* list = &gHashTb[idx].list;
-            list->insertByOrder(msgID);
+            retIDs[cnt++] = temp->msgId;
         }
         else
         {
-            i++;
+            break;
         }
-    }
-}
-
-// followTb에 userId1 index에 userId2를 등록한다.
-void followUser(int userID1, int userID2)
-{
-    int* followCntIdx = &gFollowTb[userID1][1000];
-    gFollowTb[userID1][*followCntIdx] = userID2;
-    (*followCntIdx)++;
-}
-
-// hashtagTb에 tagName으로 Index를 찾아서...
-int searchByHashtag(char tagName[], int retIDs[])
-{
-    int idx = find(tagName);
-    if (-1 == idx)
-    {
-        return 0;
-    }
-
-    List* list = &gHashTb[idx].list;
-    int cnt = 0;
-    for (Node* temp = list->head.next; temp != &(list->tail); temp = temp->next)
-    {
-        if (cnt >= 5) break;
-        retIDs[cnt++] = temp->msgId;
     }
     return cnt;
 }
 
-// userId가 follow하고 있는 userTb 요소들을 모은다.
-// 우선순위에 따라 정렬한다. -> 힙정렬 아니면 병합정렬로
+void mergeSort(int s, int e)
+{
+    // divide
+    int m = (s + e) / 2;
+    if (s < e)
+    {
+        mergeSort(s, m);
+        mergeSort(m + 1, e);
+    }
+
+    // merge
+    int i = s;
+    int j = m + 1;
+    int k = s;
+
+    while (i <= m && j <= e)
+    {
+        if (compare(gArray[i], gArray[j])) gBuffer[k++] = gArray[i++];
+        else gBuffer[k++] = gArray[j++];
+    }
+
+    while (i <= m) gBuffer[k++] = gArray[i++];
+    while (j <= e) gBuffer[k++] = gArray[j++];
+
+    for (int i = s; i <= e; i++)
+    {
+        gArray[i] = gBuffer[i];
+    }
+}
+
 int getMessages(int userID, int retIDs[])
 {
-    gHeap.init();
-
-    int searchCnt = 0;
-    for (Node* temp = gUserTb[userID].head.next; temp != &gUserTb[userID].tail; temp = temp->next, searchCnt++)
+    int c = 0;
+    // follow table을 한 바퀴 돈다.
+    for (int i = 0; i <= gFollowTb[userID][0]; i++)
     {
-        if (searchCnt >= 5) break;
-        gHeap.heapPush(temp->msgId);
-    }
+        int uId;
+        if (0 == i) uId = userID;
+        else        uId = gFollowTb[userID][i];
 
-    for (int i = 0; i < gFollowTb[userID][1000]; i++)
-    {
-        int userId = gFollowTb[userID][i];
-        int searchCnt = 0;
-        for (Node* temp = gUserTb[userId].head.next; temp != &gUserTb[userId].tail; temp = temp->next, searchCnt++)
+        List* pList = &gUserListTb[uId];
+        int cnt = 0;
+        for (Node* temp = pList->head.next; temp != &(pList->tail); temp = temp->next)
         {
-            if (searchCnt >= 5) break;
-            gHeap.heapPush(temp->msgId);
+            if (cnt < 5)
+            {
+                gArray[c++] = temp->msgId;
+                cnt++;
+            }
+            else
+            {
+                break;
+            }
         }
     }
-    
+
+    mergeSort(0, c-1);
+
     int cnt = 0;
-    while ((0 != gHeap.heapSize) && (cnt < 5)) //탈출조건: heap이 비어있거나 cnt가 5이다.
+    for (int i = 0; i < c; i++)
     {
-        int value;
-        gHeap.heapPop(&value);
-        retIDs[cnt++] = value;
+        if (cnt < 5)
+        {
+            retIDs[i] = gArray[i];
+            cnt++;
+        }
+        else
+        {
+            break;
+        }
     }
     return cnt;
 }
